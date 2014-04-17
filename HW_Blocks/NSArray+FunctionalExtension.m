c1e20d2ecaae087851a4a9ca9c5a05b9b6471ceb
nastya13 <nastya94-94.08@mail.ru>
2014-04-16 17:38:55 -0700

    Task completed :)

--- HW_Blocks/NSArray+FunctionalExtension.m
+++ HW_Blocks/NSArray+FunctionalExtension.m
@@ -10,19 +10,45 @@
 
 @implementation NSArray (FunctionalExtension)
 
+
 - (NSArray*)ex_map:(id(^)(id obj))mapBlock
 {
-    return nil;
+    
+    NSArray *newArray = [NSArray new];
+    NSMutableArray *advanceArray = [NSMutableArray new];
+    
+    
+    for (id obj in self) {
+        [advanceArray addObject:mapBlock(obj)];
+    }
+    newArray = [advanceArray copy];
+    return newArray;
 }
 
 - (NSArray*)ex_filter:(BOOL(^)(id obj))filterBlock
 {
-    return nil;
+    NSArray *newArray = [NSArray new];
+    NSMutableArray *advanceArray = [NSMutableArray new];
+    
+    
+    for (id obj in self) {
+        if (filterBlock(obj) == YES)
+            [advanceArray addObject:obj];
+        }
+    newArray = [advanceArray copy];
+    return newArray;
 }
 
 - (id)ex_sum:(id)initialValue withBlock:(id(^)(id sum, id obj))sumBlock
 {
-    return nil;
+    id result = initialValue;
+    NSLog(@"%@", result);
+    for (id obj in self) {
+        result = sumBlock(result, obj);
+        NSLog(@"%@", result);
+    }
+    
+    return result;
 }
 
 @end
