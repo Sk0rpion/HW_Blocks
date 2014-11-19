1d79d5ba2178588dc5b86b216d41d6026a5a2b0b
Irinoise <irinoise@mail.ru>
2014-11-19 17:10:19 +0300

    Какой-то очередной не очень информативный коммит

--- HW_Blocks/NSArray+FunctionalExtension.m
+++ HW_Blocks/NSArray+FunctionalExtension.m
@@ -12,17 +12,37 @@
 
 - (NSArray*)ex_map:(id(^)(id obj))mapBlock
 {
-    return nil;
+    NSMutableArray *mapArray = [NSMutableArray new];
+    [self enumerateObjectsUsingBlock:^(id arrObj, NSUInteger idx, BOOL *stop) {
+        
+        arrObj = mapBlock(arrObj);
+        [mapArray addObject:arrObj];
+        
+    }];
+    return mapArray;
 }
 
 - (NSArray*)ex_filter:(BOOL(^)(id obj))filterBlock
 {
-    return nil;
+    NSMutableArray *filterArr = [NSMutableArray new];
+    [self enumerateObjectsUsingBlock:^(id arrObj, NSUInteger idx, BOOL *stop) {
+        
+        if(filterBlock(arrObj)){
+            
+            [filterArr addObject:arrObj];
+        }
+    }];
+    return filterArr;
 }
 
 - (id)ex_sum:(id)initialValue withBlock:(id(^)(id sum, id obj))sumBlock
 {
-    return nil;
+    __block id sumResult = initialValue;
+    [self enumerateObjectsUsingBlock:^(id arrObj, NSUInteger idx, BOOL *stop) {
+        
+        sumResult = sumBlock(sumResult,arrObj);
+    }];
+    return sumResult;
 }
 
 @end
